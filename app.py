a14dfee674f861747fd5905c67f864d3043e9cfb
kwisener01 <158852969+kwisener01@users.noreply.github.com>
2025-04-27 13:13:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -28,8 +28,8 @@ def calculate_bayesian_forecast(df):
     df['ATR'] = df['TR'].rolling(atr_length).mean()
 
     hl2 = (df['High'] + df['Low']) / 2
-    df['UpperBand'] = hl2.add(multiplier * df['ATR'])
-    df['LowerBand'] = hl2.subtract(multiplier * df['ATR'])
+    df['UpperBand'] = hl2 + (multiplier * df['ATR'])
+    df['LowerBand'] = hl2 - (multiplier * df['ATR'])
     df['Supertrend'] = 0
     df['Direction'] = 0
 
